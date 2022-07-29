14597e9aa72c1c9ebc0f052145b909f70977d659
Your Name <you@example.com>
2022-07-29 09:53:27 +0530

    mappings

--- student-sai-application/student-sai-application/src/main/java/com/student/entity/Student.java
+++ student-sai-application/student-sai-application/src/main/java/com/student/entity/Student.java
@@ -1,10 +1,18 @@
 package com.student.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -32,4 +40,17 @@ public class Student {
 	private String loginId;
 	@Column(name = "password")
 	private String password;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id")
+	private Address address;
+
+	@ManyToOne(cascade = CascadeType.ALL, targetEntity = College.class)
+	@JoinColumn(name = "college_Id")
+	private College college;
+
+	@ManyToMany(cascade = CascadeType.ALL, targetEntity = Courses.class)
+	@JoinTable(name = "student_course_details", joinColumns = {
+			@JoinColumn(name = "student_id") }, inverseJoinColumns = { @JoinColumn(name = "course_id") })
+	private List<Courses> courses;
 }
